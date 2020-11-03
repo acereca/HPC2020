2f2a98375eccdfdc274346584a49f4794a24ed68
Daniel Barley <daniel_barley@aim.com>
2020-11-03 17:58:53 +0100

    Add plotting template `eval.py`

---
+++ template/eval.py
@@ -0,0 +1,7 @@
+import matplotlib as mpl
+from matplotlib import rc as rc
+from matplotlib import pyplot as plt
+
+rc('text', usetex=True)
+rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+plt.style.use('ggplot')
