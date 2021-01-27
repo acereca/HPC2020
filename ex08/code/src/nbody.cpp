3b347f7066df803b8c5823734444d34c032b08e0
Daniel Barley <daniel_barley@aim.com>
2021-01-28 00:36:50 +0100

    Update nbody.cpp and add vis command to docs Makefile

--- ex08/code/src/nbody.cpp
+++ ex08/code/src/nbody.cpp
@@ -1,260 +1,275 @@
+#include <mpi.h>
+#include <stdlib.h>
+
 #include <array>
 #include <chrono>
 #include <cmath>
 #include <fstream>
 #include <iostream>
-#include <mpi.h>
 #include <random>
-#include <stdlib.h>
 #include <string>
 
 #include "../../../libs/argparse/argparse.hpp"
 #include "../../../libs/mpi_debug/debug.hpp"
 
-int DELTA_T = 100;          // s
-constexpr double GAMMA = 6.673e-11; // N m^2 kg^-2
-constexpr double EPSILON = 1e-30;   // softening factor
-std::random_device
-    rd; // Will be used to obtain a seed for the random number engine
-std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-std::uniform_real_distribution<> mass_distrib(.9e1, 1e1);
-std::uniform_real_distribution<>
-    space_distrib(-1e3, 1e3); // start within 25km of eachother
+int DELTA_T = 100;		     // s
+constexpr double GAMMA = 6.673e-11;  // N m^2 kg^-2
+constexpr double EPSILON = 1e-50;    // softening factor
+// Will be used to obtain a seed for the random number engine
+std::random_device rd;
+// Standard mersenne_twister_engine seeded with rd()
+std::mt19937 gen(rd());
+std::uniform_real_distribution<> mass_distrib(1e24, 9e24);
+std::uniform_real_distribution<> space_distrib(0, 1e11);
 
 struct bbuffer {
-  std::vector<double> data;
+	std::vector<double> data;
 
-  double *mass;
-  double *posX;
-  double *posY;
-  double *posZ;
+	double *mass;
+	double *posX;
+	double *posY;
+	double *posZ;
 
-  bbuffer() = delete;
-  bbuffer(size_t n) {
-    data.resize(4 * n, 0);
-    mass = &data[0];
-    posX = &data[1 * n];
-    posY = &data[2 * n];
-    posZ = &data[3 * n];
-  }
+	bbuffer() = delete;
+	bbuffer(size_t n) {
+		data.resize(4 * n, 0);
+		mass = &data[0];
+		posX = &data[1 * n];
+		posY = &data[2 * n];
+		posZ = &data[3 * n];
+	}
 };
 
 struct bodies {
-  // contains all data for all bodies of this rank
-  // [mass_0..mass_N, posX_0..posX_N, ]
-  // double *data;
-  std::vector<double> data;
+	// contains all data for all bodies of this rank
+	// [mass_0..mass_N, posX_0..posX_N, ]
+	// double *data;
+	std::vector<double> data;
 
-  // masses for all bodies in this rank
-  double *mass;
+	// masses for all bodies in this rank
+	double *mass;
 
-  // same for positions
-  double *posX;
-  double *posY;
-  double *posZ;
+	// same for positions
+	double *posX;
+	double *posY;
+	double *posZ;
 
-  // and velocities
-  double *velX;
-  double *velY;
-  double *velZ;
+	// and velocities
+	double *velX;
+	double *velY;
+	double *velZ;
 
-  size_t n;
+	size_t n;
 
-  bodies() = delete;
-  bodies(size_t n, size_t nulled = 0) : n(n) {
-    // data = static_cast<double *>(calloc(7 * n, sizeof(double)));
-    data.resize(7 * n, 0);
-    mass = &data[0];
+	bodies() = delete;
+	bodies(size_t n, size_t nulled = 0) : n(n) {
+		// data = static_cast<double *>(calloc(7 * n, sizeof(double)));
+		data.resize(7 * n, 0);
+		mass = &data[0];
 
-    // pointers to sub (we now this is hacky,
-    //   would require views, since c++17, we are stuck with c++11)
-    posX = &data[1 * n];
-    posY = &data[2 * n];
-    posZ = &data[3 * n];
-    velX = &data[4 * n];
-    velY = &data[5 * n];
-    velZ = &data[6 * n];
+		// pointers to sub (we now this is hacky,
+		// would require views from c++17, we are stuck with c++11)
+		posX = &data[1 * n];
+		posY = &data[2 * n];
+		posZ = &data[3 * n];
+		velX = &data[4 * n];
+		velY = &data[5 * n];
+		velZ = &data[6 * n];
 
-    for (size_t i = 0; i < (n - nulled); i++) {
-      mass[i] = mass_distrib(gen);
-      // posX[i] = space_distrib(gen);
-      // posY[i] = space_distrib(gen);
-      // posZ[i] = space_distrib(gen);
-      posX[i] = i*10;
-      posY[i] = i*10;
-      posZ[i] = i*10;
-    }
-
-  }
-  void print_to_file(std::string fd, size_t step) {
-    std::fstream fs;
-    if (step == 0) {
-      fs = std::fstream(fd, fs.out | fs.trunc);
-    } else {
-      fs = std::fstream(fd, fs.out | fs.app);
-    }
-    if (!fs.is_open())
-      std::fprintf(stderr, "failed to open file %s\n", fd.c_str());
-    else {
-      fs << "#step; "
-         << "mass [kg]; "
-         << "pos.x; "
-         << "pos.y; "
-         << "pos.z;" << std::endl;
-      for (size_t i = 0; i < n; ++i) {
-        fs << step << ", " << mass[i] << ", " << posX[i] << ", " << posY[i] << ", " << posZ[0]
-           << "\n";
-      }
-    }
-    fs.close();
-  }
+		for (size_t i = 0; i < (n - nulled); i++) {
+			mass[i] = mass_distrib(gen);
+			posX[i] = space_distrib(gen);
+			posY[i] = space_distrib(gen);
+			posZ[i] = space_distrib(gen);
+		}
+	}
+	void print_to_file(std::string fd, size_t step) {
+		std::fstream fs;
+		if (step == 0) {
+			fs = std::fstream(fd, fs.out | fs.trunc);
+		} else {
+			fs = std::fstream(fd, fs.out | fs.app);
+		}
+		if (!fs.is_open())
+			std::fprintf(stderr, "failed to open file %s\n",
+				     fd.c_str());
+		else {
+			fs << "#step; "
+			   << "mass [kg]; "
+			   << "pos.x; "
+			   << "pos.y; "
+			   << "pos.z;" << std::endl;
+			for (size_t i = 0; i < n; ++i) {
+				fs << step << ", " << mass[i] << ", " << posX[i]
+				   << ", " << posY[i] << ", " << posZ[0]
+				   << "\n";
+			}
+		}
+		fs.close();
+	}
 };
 
 size_t ceil(size_t a, size_t b) { return (a + b - 1) / b; }
 
 struct leap_frog_iterator {
-  // size_t step_ring = 0;
-  const size_t size_ring;
-  const size_t n;
-  bodies const &b;
+	// size_t step_ring = 0;
+	const size_t size_ring;
+	const size_t n;
+	bodies const &b;
 
-  std::vector<std::array<double, 3>> acc;
+	std::vector<std::array<double, 3>> acc;
 
-  leap_frog_iterator() = delete;
-  leap_frog_iterator(size_t size_ring, size_t n, bodies &b)
-      : size_ring(size_ring), n(n), b(b) {
-    acc.resize(n, {0, 0, 0});
-    // b = b;
-  };
+	leap_frog_iterator() = delete;
+	leap_frog_iterator(size_t size_ring, size_t n, bodies &b)
+	    : size_ring(size_ring), n(n), b(b) {
+		acc.resize(n, {0, 0, 0});
+	};
 
-  void update() {
-    for (size_t i = 0; i < n; i++) {
-      b.posX[i] += b.velX[i] * DELTA_T;
-      b.posY[i] += b.velY[i] * DELTA_T;
-      b.posZ[i] += b.velZ[i] * DELTA_T;
+	void update() {
+		for (size_t i = 0; i < n; i++) {
+			b.posX[i] += b.velX[i] * DELTA_T;
+			b.posY[i] += b.velY[i] * DELTA_T;
+			b.posZ[i] += b.velZ[i] * DELTA_T;
 
-      b.velX[i] += acc[i][0] * DELTA_T;
-      b.velY[i] += acc[i][1] * DELTA_T;
-      b.velZ[i] += acc[i][2] * DELTA_T;
-    }
-    acc.clear();
-    acc.resize(n, {0, 0, 0});
-  }
+			b.velX[i] += acc[i][0] * DELTA_T;
+			b.velY[i] += acc[i][1] * DELTA_T;
+			b.velZ[i] += acc[i][2] * DELTA_T;
+		}
+		acc.clear();
+		acc.resize(n, {0, 0, 0});
+	}
 
-  void operator()(bbuffer &msg_objs) {
-    for (size_t i = 0; i < n; i++) {
-      for (size_t j = 0; j < n; j++) {
-        double dist_x = - (b.posX[i] - msg_objs.posX[j]);
-        double dist_y = - (b.posY[i] - msg_objs.posY[j]);
-        double dist_z = - (b.posZ[i] - msg_objs.posZ[j]);
-        double dist_sq = dist_x * dist_x + dist_y * dist_y + dist_z * dist_z + EPSILON;
-        acc[i][0] +=
-            -GAMMA * (msg_objs.mass[j]) / dist_sq * dist_x / std::sqrt(dist_sq);
-        acc[i][1] +=
-            -GAMMA * (msg_objs.mass[j]) / dist_sq * dist_y / std::sqrt(dist_sq);
-        acc[i][2] +=
-            -GAMMA * (msg_objs.mass[j]) / dist_sq * dist_z / std::sqrt(dist_sq);
-      }
-    }
-  }
+	void operator()(bbuffer &msg_objs) {
+		for (size_t i = 0; i < n; i++) {
+			for (size_t j = 0; j < n; j++) {
+				double rX = (b.posX[i] - msg_objs.posX[j]);
+				double rY = (b.posY[i] - msg_objs.posY[j]);
+				double rZ = (b.posZ[i] - msg_objs.posZ[j]);
+				double dist_sq =
+				    rX * rX + rY * rY + rZ * rZ + EPSILON;
+				double distX = rX / std::sqrt(dist_sq);
+				double distY = rY / std::sqrt(dist_sq);
+				double distZ = rZ / std::sqrt(dist_sq);
+				acc[i][0] =
+				    -(GAMMA * msg_objs.mass[j] * distX) /
+				    dist_sq;
+				acc[i][1] =
+				    -(GAMMA * msg_objs.mass[j] * distY) /
+				    dist_sq;
+				acc[i][2] =
+				    -(GAMMA * msg_objs.mass[j] * distZ) /
+				    dist_sq;
+			}
+		}
+	}
 };
 
 int main(int argc, char const *argv[]) {
-  // init stuff
-  argparse::ArgumentParser ap;
-  ap.addArgument("-n", "--num-objects", 1, false);
-  ap.addArgument("-i", "--iterations", 1, false);
-  ap.addArgument("-t", "--tstep", 1, false);
-  ap.parse(argc, argv);
+	// init stuff
+	argparse::ArgumentParser ap;
+	ap.addArgument("-n", "--num-objects", 1, false);
+	ap.addArgument("-i", "--iterations", 1, false);
+	ap.addArgument("-t", "--tstep", 1, false);
+	ap.parse(argc, argv);
 
-  size_t size = ap.retrieve<size_t>("num-objects");
-  size_t iterations = ap.retrieve<size_t>("iterations");
-  DELTA_T = ap.retrieve<size_t>("tstep");
+	size_t size = ap.retrieve<size_t>("num-objects");
+	size_t iterations = ap.retrieve<size_t>("iterations");
+	DELTA_T = ap.retrieve<size_t>("tstep");
 
-  int processes = 1;
-  int rank = 0;
-  int prev = 0;
-  int next = 0;
+	// int processes = 1;
+	// int rank = 0;
+	// int prev = 0;
+	// int next = 0;
+	int processes;
+	int rank;
+	int prev;
+	int next;
 
-  MPI::Init();
-  MPI_Comm_size(MPI_COMM_WORLD, &processes);
+	MPI::Init();
+	MPI_Comm_size(MPI_COMM_WORLD, &processes);
 
-  MPI_Comm WORLD_1D;
-  const int dims[] = {processes};
-  const int periods[] = {true};
+	MPI_Comm WORLD_1D;
+	const int dims[] = {processes};
+	const int periods[] = {true};
 
-  MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, false, &WORLD_1D);
+	MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, false, &WORLD_1D);
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0) {
-    MPI_DBG::enable_debugging(rank);
-  }
-  int coord[1];
-  MPI_Cart_coords(WORLD_1D, rank, 1, coord);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0) {
+		MPI_DBG::enable_debugging(rank);
+	}
+	int coord[1];
+	MPI_Cart_coords(WORLD_1D, rank, 1, coord);
 
-  size_t bodies_per_rank = ceil(size, processes);
+	size_t bodies_per_rank = ceil(size, processes);
 
-  bodies objects(bodies_per_rank);
-  std::array<bbuffer, 2> bbuffs{bodies_per_rank, bodies_per_rank};
+	bodies objects(bodies_per_rank);
+	std::array<bbuffer, 2> bbuffs{bodies_per_rank, bodies_per_rank};
 
-  if (coord[0] == (processes - 1)) {
-    objects = bodies(bodies_per_rank, (bodies_per_rank - (size % processes)) % bodies_per_rank);
+	if (coord[0] == (processes - 1)) {
+		objects = bodies(
+		    bodies_per_rank,
+		    (bodies_per_rank - (size % processes)) % bodies_per_rank);
+	}
 
-    // iii iii iii i00
-  }
+	leap_frog_iterator lfi(processes, bodies_per_rank, objects);
 
-  leap_frog_iterator lfi(processes, bodies_per_rank, objects);
+	auto t_start = std::chrono::high_resolution_clock::now();
+	// compute section
+	// MPI_irecv ->0
 
-  auto t_start = std::chrono::high_resolution_clock::now();
-  // compute section
-  // MPI_irecv ->0
+	MPI_Cart_shift(WORLD_1D, 0, -1, &rank, &prev);
+	MPI_Cart_shift(WORLD_1D, 0, 1, &rank, &next);
 
-  MPI_Cart_shift(WORLD_1D, 0, -1, &rank, &prev);
-  MPI_Cart_shift(WORLD_1D, 0, 1, &rank, &next);
+	MPI_Request sig_recv, sig_send;
 
-  MPI_Request sig_recv, sig_send;
+	MPI_Irecv(&bbuffs[0].data[0], bodies_per_rank * 4, MPI_DOUBLE, prev, 0,
+		  WORLD_1D, &sig_recv);
+	MPI_Isend(&objects.data[0], bodies_per_rank * 4, MPI_DOUBLE, next, 0,
+		  WORLD_1D, &sig_send);
 
-  MPI_Irecv(&bbuffs[0].data[0], bodies_per_rank * 4, MPI_DOUBLE, prev, 0, WORLD_1D,
-            &sig_recv);
-  MPI_Isend(&objects.data[0], bodies_per_rank * 4, MPI_DOUBLE, next, 0,
-            WORLD_1D, &sig_send);
-
-  for (size_t time_step = 0; time_step < iterations; time_step++) {
-    // for processes-1
-    for (size_t loop_step = 0; loop_step < (processes - 1); loop_step++) {
-      // MPI_wait
-      MPI_Wait(&sig_recv, NULL);
-      // MPI_isend 0->
-      MPI_Wait(&sig_send, NULL);
-      MPI_Isend(&bbuffs[loop_step % 2].data[0], bodies_per_rank * 4, MPI_DOUBLE, next,
-                0, WORLD_1D, &sig_send);
-      // MPI-irecv -> 1
-      MPI_Irecv(&bbuffs[(loop_step + 1) % 2].data[0], bodies_per_rank * 4, MPI_DOUBLE,
-                prev, 0, WORLD_1D, &sig_recv);
-      // compute
-      lfi(bbuffs[loop_step % 2]);
-      lfi.update();
-    }
+	for (size_t time_step = 0; time_step < iterations; time_step++) {
+		// for processes-1
+		for (size_t loop_step = 0;
+		     loop_step < static_cast<size_t>(processes - 1);
+		     loop_step++) {
+			// MPI_wait
+			MPI_Wait(&sig_recv, NULL);
+			// MPI_isend 0->
+			MPI_Wait(&sig_send, NULL);
+			MPI_Isend(&bbuffs[loop_step % 2].data[0],
+				  bodies_per_rank * 4, MPI_DOUBLE, next, 0,
+				  WORLD_1D, &sig_send);
+			// MPI-irecv -> 1
+			MPI_Irecv(&bbuffs[(loop_step + 1) % 2].data[0],
+				  bodies_per_rank * 4, MPI_DOUBLE, prev, 0,
+				  WORLD_1D, &sig_recv);
+			// compute
+			lfi(bbuffs[loop_step % 2]);
+			lfi.update();
+		}
 
 #ifdef VERBOSE
-    objects.print_to_file("./out/bodies_r" + std::to_string(coord[0]), time_step);
+		objects.print_to_file(
+		    "./out/bodies_r" + std::to_string(coord[0]), time_step);
 #endif
-    MPI_Wait(&sig_recv, NULL);
-    MPI_Wait(&sig_send, NULL);
-    MPI_Irecv(&bbuffs[0].data[0], bodies_per_rank * 4, MPI_DOUBLE, prev, 0, WORLD_1D,
-              &sig_recv);
-    MPI_Isend(&objects.data[0], bodies_per_rank * 4, MPI_DOUBLE, next, 0,
-              WORLD_1D, &sig_send);
-  }
-  auto t_end = std::chrono::high_resolution_clock::now();
-  auto t_diff =
-      std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start)
-          .count();
+		MPI_Wait(&sig_recv, NULL);
+		MPI_Wait(&sig_send, NULL);
+		MPI_Irecv(&bbuffs[0].data[0], bodies_per_rank * 4, MPI_DOUBLE,
+			  prev, 0, WORLD_1D, &sig_recv);
+		MPI_Isend(&objects.data[0], bodies_per_rank * 4, MPI_DOUBLE,
+			  next, 0, WORLD_1D, &sig_send);
+	}
+	auto t_end = std::chrono::high_resolution_clock::now();
+	auto t_diff = std::chrono::duration_cast<std::chrono::microseconds>(
+			  t_end - t_start)
+			  .count();
 
-  if (coord[0] == 0) {
-    std::cout << size << ", " << processes << ", " << t_diff << ", " << iterations << "\n";
-  }
+	if (coord[0] == 0) {
+		std::cout << size << ", " << processes << ", " << t_diff << ", "
+			  << iterations << "\n";
+	}
 
-  MPI::Finalize();
-  return 0;
+	MPI::Finalize();
+	return 0;
 }
--- ex08/docs/eval/nbody_vis.py
+++ ex08/docs/eval/nbody_vis.py
@@ -23,55 +23,89 @@ for f in glob.glob("../code/out/bodies_r*"):
     )
 
 
-print(df[df['step'] == 0].iloc[0], df[df['step'] == 199].iloc[0])
-print(df[df['step'] == 0].iloc[1], df[df['step'] == 199].iloc[1])
+print(df[df['step'] == 0].iloc[0], df[df['step'] == df['step'].max()].iloc[0])
 
-fig, axs = plt.subplots(3, 1)
+# fig, axs = plt.subplots(1, 3, figsize=(15,5))
+
+# i = 0
+# ims = []
+
+# ax = axs[0]
+# ims.append(
+    # ax.errorbar(
+        # df[df['step'] == i]['pos.x'],
+        # df[df['step'] == i]['pos.y'],
+        # fmt=".",
+        # # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
+    # )[0]
+# )
+# ax.set_xlim([df['pos.x'].min(),df['pos.x'].max()])
+# ax.set_ylim([df['pos.y'].min(),df['pos.y'].max()])
+
+# ax = axs[1]
+# ims.append(
+    # ax.errorbar(
+        # df[df['step'] == i]['pos.x'],
+        # df[df['step'] == i]['pos.z'],
+        # fmt=".",
+        # # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
+    # )[0]
+# )
+# ax.set_xlim([df['pos.x'].min(),df['pos.x'].max()])
+# ax.set_ylim([df['pos.z'].min(),df['pos.z'].max()])
+
+# ax = axs[2]
+# ims.append(
+    # ax.errorbar(
+        # df[df['step'] == i]['pos.y'],
+        # df[df['step'] == i]['pos.z'],
+        # fmt=".",
+        # # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
+    # )[0]
+# )
+# ax.set_xlim([df['pos.y'].min(),df['pos.y'].max()])
+# ax.set_ylim([df['pos.z'].min(),df['pos.z'].max()])
+
+# # animation function, creates the plot frame by frame (i)
+# def animate(i):
+    # ims[0].set_data(df[df['step'] == i]['pos.x'], df[df['step'] == i]['pos.y'])
+    # ims[1].set_data(df[df['step'] == i]['pos.x'], df[df['step'] == i]['pos.z'])
+    # ims[2].set_data(df[df['step'] == i]['pos.y'], df[df['step'] == i]['pos.z'])
+    # print(f"Progress: {round(100*i/df['step'].max())}% ({i}/{df['step'].max()})", end="\r")
+
+# fps = 3
+# anim = man.FuncAnimation(fig, animate, frames=df['step'].max(),
+                               # interval=1000/fps, blit=False)
+# plt.tight_layout()
+# # ax.set_xticks([])
+# # ax.set_yticks([])
+# print("Stitching ...")
+# anim.save('img/nbody_vis.mp4', fps=fps, dpi=300)
+# print("Done!")
+
+fig = plt.figure();
+ax = fig.add_subplot(111, projection='3d')
 
 i = 0
-ims = []
-
-ax = axs[0]
-ims.append(
-    ax.errorbar(
+im, = ax.plot(
         df[df['step'] == i]['pos.x'],
         df[df['step'] == i]['pos.y'],
-        fmt=".",
-        # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
-    )[0]
-)
-
-ax = axs[1]
-ims.append(
-    ax.errorbar(
-        df[df['step'] == i]['pos.x'],
         df[df['step'] == i]['pos.z'],
-        fmt=".",
+        marker='o',
+        linestyle='none'
         # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
-    )[0]
-)
+    )
+# ax.set_xlim([df['pos.x'].min(),df['pos.x'].max()])
+# ax.set_ylim([df['pos.y'].min(),df['pos.y'].max()])
 
-ax = axs[2]
-ims.append(
-    ax.errorbar(
-        df[df['step'] == i]['pos.y'],
-        df[df['step'] == i]['pos.z'],
-        fmt=".",
-        # ms=np.log(df[df['step'] == i]['mass [kg]']).tolist()
-    )[0]
-)
 
-for ax in axs:
-    ax.set_xlim([-1e2,1e2])
-    ax.set_ylim([-1e2,1e2])
 # animation function, creates the plot frame by frame (i)
 def animate(i):
-    ims[0].set_data(df[df['step'] == i]['pos.x'], df[df['step'] == i]['pos.y'])
-    ims[1].set_data(df[df['step'] == i]['pos.x'], df[df['step'] == i]['pos.z'])
-    ims[2].set_data(df[df['step'] == i]['pos.y'], df[df['step'] == i]['pos.z'])
+    im.set_data(df[df['step'] == i]['pos.x'], df[df['step'] == i]['pos.y'])
+    im.set_3d_properties(df[df['step'] == i]['pos.z'])
     print(f"Progress: {round(100*i/df['step'].max())}% ({i}/{df['step'].max()})", end="\r")
 
-fps = 3
+fps = df['step'].max()/20
 anim = man.FuncAnimation(fig, animate, frames=df['step'].max(),
                                interval=1000/fps, blit=False)
 plt.tight_layout()
